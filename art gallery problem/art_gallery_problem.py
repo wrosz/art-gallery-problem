3897e06d4130f4c7f49e7b694bf3c42bae2c077b
Wojtek Roszkowski <17rosz17@gmail.com>
2024-11-05 23:52:17 +0100

    Podzielenie kodu na pliki, ulepszenie interfejsu (dodanie osi współrzędnych i siatki), sprawdzanie samoprzecięć w wielokącie, zmiana sposobu zapisywania współrzędnych

--- art gallery problem/art_gallery_problem.py
+++ art gallery problem/art_gallery_problem.py
@@ -1,1 +1,86 @@
-from user_input_polygon import *
+# Plik główny, zawierający main()
+
+import pygame
+from rysowanie_okna import *
+from funkcje_pomocnicze import *
+
+
+def main():
+
+    # Inicjalizacja i rysowanie okna
+    screen = init_window()
+    canvas_rect = draw_canvas_area(screen)
+    komunikat_rect = draw_komunikat_area(screen)
+    draw_grid(screen)
+    draw_axes(screen)
+
+    # Główna pętla programu
+    running = True
+
+    # zmienne potrzebne do wprowadzania wielokąta:
+    draw_enabled = True  # True, gdy tryb rysowania jest włączony
+    co_najmniej_trzy_punkty = False  # True, jeśli użytkownik już wprowadził co najmniej trzy wierzchołki (co pozwala zamknąc wielokąt)
+    czy_pierwszy_wierzcholek = True  # True, jeśli użytkownik nie narysował jeszcze żadnego wierzchołka
+    last_pos = None  # współrzędne poprzednio narysowanego wierzchołka
+    current_pos = None  # współrzędne aktualnie rysowanego wierzchołka
+    first_pos = None  # współrzędne pierwszego narysowanego wierzchołka
+    punkty = []  # lista przechowująca współrzędne wierzchołków wielokąta
+    point_radius=5  # promień koła oznaczającego wierzchołek wprowadzanego wielokąta
+
+    wyswietl_tekst('Narysuj pierwszy wierzchołek wielokąta, klikając myszą w dowolne miejsce w obszarze płótna.', screen, komunikat_rect)
+
+    while running:
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:  # wyjście z programu
+                running = False
+
+            if event.type == pygame.MOUSEMOTION:  # wyświetlenie współrzędnych kursora myszy na dole ekranu
+                mouse_x, mouse_y = event.pos
+                wyswietl_tekst(f'x:{mouse_x}, y:{mouse_y}', screen, wspolrzedne_rect, font_size=15, background_color=GREY)
+
+            # WPROWADZANIE PRZEZ UŻYTKOWNIKA WIELOKĄTA
+            if draw_enabled:
+
+                if event.type == pygame.MOUSEBUTTONDOWN:  # obsługa klikania przycisku myszy
+                    mouse_x, mouse_y = event.pos # pobranie współrzędnych w miejscu, gdzie kliknął użytkownik
+                    
+                    if canvas_rect.collidepoint(mouse_x, mouse_y):  # gdy kliknięto w obszarze płótna,
+                                                                    # pobrane współrzędne są wierzchołkiem wielokąta
+
+                        # gdy zamykamy wielokąt, wyłączamy tryb rysowania i zapisujemy plik ze współrzędnymi punktów
+                        if circle_collidepoint(first_pos, point_radius, (mouse_x, mouse_y)) and co_najmniej_trzy_punkty:
+                            draw_enabled = False
+                            pygame.draw.line(screen, RED, last_pos, first_pos, width=2)
+                            zapisz_wspolrzedne(punkty)
+                            wyswietl_tekst(['Wielokąt zamknięty!', 'Aby wyjść z programu, zamknij okno.'], screen, komunikat_rect)
+
+                        else:
+                            current_pos_osiowe = wspol_osiowe((mouse_x, mouse_y), canvas_rect)
+                            if sprawdz_samoprzeciecia(current_pos_osiowe, punkty):  # sprawdza, czy rysowany bok wielokąta nie będzie przecinał figury
+                                wyswietl_tekst(["Nie można nanieść punktu, wielokąt będzie zawierał samoprzecięcia.", "Zaznacz inny punkt."], screen, komunikat_rect)
+                            else:  # jeśli nie, to rysujemy nowy bok wielokąta i dodajemy punkt do listy
+                                current_pos = (mouse_x, mouse_y)
+                                pygame.draw.circle(screen, RED, current_pos, point_radius)
+                                punkty.append(current_pos_osiowe)  # dołączamy do listy współrzędne rysowanego punktu, zgodnie z pozycją kursora myszy wyświetlaną na ekranie
+                                if czy_pierwszy_wierzcholek:
+                                    first_pos = current_pos
+                                    czy_pierwszy_wierzcholek = False
+                                    wyswietl_tekst(['Zaznacz kolejne wierzchołki wielokąta.',
+                                                    'Następnie zamknij wielokąt, klikając pierwszy narysowany wierzchołek.'], screen, komunikat_rect)
+                                else:
+                                    pygame.draw.line(screen, RED, last_pos, current_pos, width=2)
+                                last_pos = current_pos
+
+                                if (not co_najmniej_trzy_punkty) and len(punkty) >= 3:  # w momencie, gdy rysowany jest trzeci punkt,
+                                                                                        # zmieniamy zmienną co_najmniej_trzy_punkty na False
+                                    co_najmniej_trzy_punkty = True
+            
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
---
+++ art gallery problem/rysowanie_okna.py
@@ -0,0 +1,96 @@
+# Plik zawierający funkcje potrzebne do stworzenia okna w Pygame i narysowania jego elementów, wywołane w main().
+# Zawiera też zmienne konfigurujące wielkość i położenie poszczególnych elementów interfejsu oraz krotki definiujące podstawowe kolory.
+
+import pygame
+from funkcje_pomocnicze import wspol_osiowe, wyswietl_tekst
+
+
+# ---------------------------- KONFIGURACJA ----------------------------
+
+# Kolory
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREY = (200, 200, 200)
+RED = (255, 0, 0)
+
+# Wymiary elementów okna
+canvas_width, canvas_height = 700, 500  # Wymiary płótna
+komunikat_width, komunikat_height = canvas_width, 40  # Wymiary komunikatów wyświetlanych na górze okna
+odstep_gora, odstep_boki, odstep_dol = 10, 40, 80  # Odstępy elementów od krawędzi
+
+# Wyliczenia wymiarów okna
+window_width = canvas_width + 2 * odstep_boki
+window_height = canvas_height + komunikat_height + 2 * odstep_gora + odstep_dol
+
+# Współrzędne elementów
+canvas_rect = pygame.Rect(odstep_boki, komunikat_height + 2 * odstep_gora, canvas_width, canvas_height)
+komunikat_rect = pygame.Rect(odstep_boki, odstep_gora, komunikat_width, komunikat_height)
+wspolrzedne_rect = pygame.Rect(odstep_boki, window_height - odstep_dol/2, canvas_width, odstep_dol/2)
+
+
+# ---------------------------- FUNKCJE ----------------------------
+
+def init_window():
+    """Inicjalizuje pygame i tworzy okno."""
+    pygame.init()
+    screen = pygame.display.set_mode((window_width, window_height))
+    screen.fill(GREY)
+    return screen
+
+
+def draw_canvas_area(screen):
+    """Rysuje główne płótno na obszarze canvas_rect."""
+    pygame.draw.rect(screen, WHITE, canvas_rect)
+    return canvas_rect
+
+
+def draw_komunikat_area(screen):
+    """Rysuje obszar na komunikaty na górze okna."""
+    pygame.draw.rect(screen, WHITE, komunikat_rect)
+    return komunikat_rect
+
+
+def draw_grid(screen):
+    """Rysuje siatkę na płótnie."""
+    for x in range(0, canvas_width + 1, 20):  # Linie pionowe siatki
+        x_pos = canvas_rect.left + x
+        pygame.draw.line(screen, GREY, (x_pos, canvas_rect.bottom), (x_pos, canvas_rect.top), 1)
+    for y in range(0, canvas_height + 1, 20):  # Linie poziome siatki
+        y_pos = canvas_rect.bottom - y
+        pygame.draw.line(screen, GREY, (canvas_rect.left, y_pos), (canvas_rect.right, y_pos), 1)
+
+
+def draw_axes(screen):
+    """Rysuje osie X i Y oraz etykiety co 100 jednostek."""
+    # Rysowanie linii osi
+    pygame.draw.line(screen, BLACK, canvas_rect.topleft, canvas_rect.bottomleft, 2)  # Oś Y
+    pygame.draw.line(screen, BLACK, canvas_rect.bottomleft, canvas_rect.bottomright, 2)  # Oś X
+
+    font = pygame.font.SysFont(None, 24)
+
+    # Rysowanie etykiet na osi X co 100 jednostek
+    for x in range(0, canvas_width + 1, 100):
+        x_pos = canvas_rect.left + x
+        pygame.draw.line(screen, BLACK, (x_pos, canvas_rect.bottom), (x_pos, canvas_rect.top), 1)
+        label = font.render(str(x), True, BLACK)
+        screen.blit(label, (x_pos - 10, canvas_rect.bottom + 10))
+
+    # Rysowanie etykiet na osi Y co 100 jednostek
+    for y in range(100, canvas_height + 1, 100):
+        y_pos = canvas_rect.bottom - y
+        pygame.draw.line(screen, BLACK, (canvas_rect.left, y_pos), (canvas_rect.right, y_pos), 1)
+        label = font.render(str(y), True, BLACK)
+        screen.blit(label, (canvas_rect.left - 35, y_pos - 6))
+
+
+def wyswietl_wspolrzedne_kursora(screen, mouse_x, mouse_y):
+    """Wyświetla współrzędne kursora myszy jeśli znajduje się on w obszarze płótna, w przeciwnym przypadku współrzędne znikają"""
+
+    (x, y) = wspol_osiowe((mouse_x, mouse_y), canvas_rect)
+    if canvas_rect.collidepoint(mouse_x, mouse_y):
+        wyswietl_tekst(f'x:{x}, y:{y}', screen, wspolrzedne_rect, font_size=15, background_color=GREY)
+    else:
+        pygame.draw.rect(screen, GREY, wspolrzedne_rect)
+
+
+
